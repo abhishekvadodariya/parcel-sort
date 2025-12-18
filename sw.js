8649d89e2a9b7c17b79b69d9e8459934471cab80
Abhishek Vadodariya <37392803+abhishekvadodariya@users.noreply.github.com>
2025-12-18 14:41:34 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-// Change 'v1' to 'v2', 'v3', etc., whenever you update the JSON
 const CACHE_NAME = 'sorter-v2'; 
 
 const ASSETS = [
@@ -6,7 +5,12 @@ const ASSETS = [
   './index.html'
 ];
 
-// This logic deletes the OLD cache and forces the NEW one
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
@@ -14,5 +18,11 @@ self.addEventListener('activate', (event) => {
         keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
       );
     })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
